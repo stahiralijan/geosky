20eeae1ad586f3cc41bfd748c4ed0d71ce18300c
Tahir <stahiralijan@gmail.com>
2025-01-25 11:55:54 +0500

    Enqueue custom styles for GeoSky theme and enhance typography for block elements

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ function geosky_theme_enqueue_styles() {
     // Enqueue Tailwind CSS (assuming it's hosted or included in the theme)
     wp_enqueue_style( 'tailwindcss', 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css' );
     wp_enqueue_script('menu-toggle', get_template_directory_uri() . '/scripts.js', array(), null, true);
+    wp_enqueue_style( 'geosky-theme', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
+
 }
 add_action( 'wp_enqueue_scripts', 'geosky_theme_enqueue_styles' );
 
